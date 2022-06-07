7618e36a296f696be42df74b5ecfe6802a27111f
Rokas Cvirka <rokas42@gmail.com>
2022-06-07 12:28:15 +0300

    Added unit test

---
+++ FibonacciSeq.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+
+namespace Fibonacci
+{
+    public class FibonacciGen
+    {
+        public int EvenSum()
+        {
+            var fibonacciList = new List<int>(new int[] { 1, 2 });
+
+            var newValue = 0;
+            var sum = 0;
+
+
+            while (newValue < 4000000)
+            {
+                var firstNumber = fibonacciList[fibonacciList.Count - 2];
+                var secondNumber = fibonacciList[fibonacciList.Count - 1];
+
+                newValue = firstNumber + secondNumber;
+
+                fibonacciList.Add(newValue);
+
+                if (newValue % 2 == 0) sum += newValue;
+            }
+
+            return sum;
+        }
+
+        public int F3()
+        {
+
+            var f1 = 1;
+            var f2 = 2;
+            var f3 = 0;
+            var sum = 0;
+
+            while ((f3 < 4000000))
+            {
+                f3 = f1 + f2;
+                if (f3 % 2 == 0) sum += f3;
+
+                f1 = f2;
+                f2 = f3;
+
+            }
+
+            return sum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,44 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+
 
 namespace Fibonacci
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            var fibonacciList = new List<int>(new int[] { 1, 2 });
-            var evenList = new List<int>();
-            
+            var fibonacci = new FibonacciGen();
 
-            for (int i = 1; i < 100; i++)
-            {
-                var firstNumber = fibonacciList[i - 1];
-                var secondNumber = fibonacciList[i];
-                var newValue = firstNumber + secondNumber;
 
-                if ((newValue < 4000000))
-                {
-                    fibonacciList.Add(newValue);
-                }
-                else
-                {
-                    break;
-                }
-
-            }
-
-            foreach (var even in fibonacciList)
-            {
-                if (even % 2 == 0)
-                {
-                    evenList.Add(even);
-                }
-            }
-
-            Console.WriteLine(evenList.Sum());
+            Console.WriteLine(fibonacci.EvenSum().ToString() + "     " + fibonacci.F3().ToString());
         }
     }
 }
