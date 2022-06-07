20bf7d123a24262764e832e3c7fcb1a3efdded1a
Rokas Cvirka <rokas42@gmail.com>
2022-06-07 10:27:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciList = new List<int>(new int[] { 1, 2 });
+            var evenList = new List<int>();
+            
+
+            for (int i = 1; i < 100; i++)
+            {
+                var firstNumber = fibonacciList[i - 1];
+                var secondNumber = fibonacciList[i];
+                var newValue = firstNumber + secondNumber;
+
+                if ((newValue < 4000000))
+                {
+                    fibonacciList.Add(newValue);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            foreach (var even in fibonacciList)
+            {
+                if (even % 2 == 0)
+                {
+                    evenList.Add(even);
+                }
+            }
+
+            Console.WriteLine(evenList.Sum());
+        }
+    }
+}
